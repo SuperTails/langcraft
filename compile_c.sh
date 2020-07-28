9df8dd50ced38581028cd558b7b7d3dd4231501e
Carson Swoveland <sciencedude2003@gmail.com>
2020-07-27 18:20:11 -0600

    Added integration test

--- compile_c.sh
+++ compile_c.sh
@@ -1,2 +1,2 @@
-clang -v -g -m32 -O2 -c -emit-llvm mcfunction.c -o mcfunction.bc
-clang -v -g -m32 -O2 -S -emit-llvm mcfunction.c -o mcfunction.ll
+clang -v -g -m32 -c -emit-llvm mcfunction.c -o mcfunction.bc
+clang -v -g -m32 -S -emit-llvm mcfunction.c -o mcfunction.ll
